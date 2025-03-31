453b390b4f1ae86286cf36a2e2aab4986f8949a8
cryptoduke01 <egbolucheakachukwu@gmail.com>
2025-03-31 05:56:46 +0100

    Fixed Import

--- src/contexts/WalletConnectionProvider.jsx
+++ src/contexts/WalletConnectionProvider.jsx
@@ -1,1 +1,34 @@
- 
+import { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+// Import styles
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+const WalletConnectionProvider = ({ children }) => {
+  // You can also provide a custom RPC endpoint
+  const endpoint = useMemo(() => clusterApiUrl('devnet'), []);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletConnectionProvider;
